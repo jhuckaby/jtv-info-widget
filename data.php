a80516c2842bcb6f1d83a4d6f9c6de611ba5f2ea
Joseph <jhuckaby@gmail.com>
2012-02-25 10:34:54 -0800

    changed to use native JSON encoding/decoding for speed.

--- data.php
+++ data.php
@@ -5,7 +5,6 @@
 // Released under the MIT License
 // http://www.opensource.org/licenses/mit-license.php
 
-require_once "xml.php";
 $channel_id = $_GET['channel'];
 
 $cache_dir = 'cache';
@@ -16,20 +15,22 @@ if (file_exists($cache_file)) {
 	$mod = filemtime($cache_file);
 	if (time() - $mod < $cache_ttl) {
 		// cache is fresh, return immediately
-		send_response(parse_xml($cache_file));
+		send_response(json_decode(file_get_contents($cache_file), true));
 	}
 }
 
 // refresh cache
-$data = parse_xml( file_get_contents('http://api.justin.tv/api/stream/list.xml?channel='.$channel_id) );
-if (!isa_hash($data) && strlen($data)) send_response(array( 'Code' => 1, 'Description' => 'Failed to parse XML data from Justin.TV API: ' . $data ));
-if (!isa_hash($data)) $data = array(
+$data = json_decode( file_get_contents('http://api.justin.tv/api/stream/list.json?channel='.$channel_id), true );
+
+if (isset($data[0])) $data = $data[0];
+if (isset($data['stream'])) $data = $data['stream'];
+
+if (!is_array($data)) $data = array(
 	'video_bitrate' => 0,
 	'channel' => array( 'status' => '' ),
 	'channel_view_count' => 0,
 	'stream_count' => 0
 );
-if (isset($data['stream'])) $data = $data['stream'];
 
 // counting followers is tricky (thanks JTV)
 $num_followers = 0;
@@ -44,21 +45,6 @@ if (file_exists($fcache_file)) {
 	}
 }
 if (!$used_fcache) {
-	/* $offset = 0;
-	$limit = 20;
-	$done = false;
-	while (!$done) {
-		$raw = file_get_contents('http://api.justin.tv/api/channel/fans/'.$channel_id.'.xml?limit='.$limit.'&offset='.$offset);
-		$count = 0;
-		while (preg_match('/\<user\>/', $raw)) {
-			$count++;
-			$raw = preg_replace('/\<user\>/', '', $raw, 1);
-		}
-		$num_followers += $count;
-		$offset += $limit;
-		if ($count < $limit) $done = true;
-	} */
-	
 	$raw = file_get_contents( 'http://www.justin.tv/' . $channel_id );
 	if (preg_match('/\<strong\s+id\=\"channel_fans_count\"\>([\d\,]+)/', $raw, $matches)) {
 		// <strong id="channel_fans_count">16,583
@@ -74,7 +60,7 @@ if (!$used_fcache) {
 $data['num_followers'] = $num_followers;
 
 // save data to cache for subsequent requests
-$result = @file_put_contents( $cache_file, compose_xml($data, 'JTVResponse') );
+$result = @file_put_contents( $cache_file, json_encode($data) );
 if (!$result) send_response(array( 'Code' => 1, 'Description' => 'Failed to save cache file to server.  Check permissions of cache dir?' ));
 
 // send data to client
@@ -83,16 +69,9 @@ send_response($data);
 exit();
 
 function send_response($data) {
-	if (isset($_GET['format']) && preg_match('/js/i', $_GET['format']) && isset($_GET['callback'])) {
-		// js response
-		header('Content-Type: text/javascript');
-		print $_GET['callback'] . '(' . trim(preg_replace('/\;$/', '', XMLtoJavaScript($data))) . ');';
-	}
-	else {
-		// xml response
-		header('Content-Type: text/xml');
-		print compose_xml( $data, 'JTVResponse' );
-	}
+	// js response
+	header('Content-Type: text/javascript');
+	print $_GET['callback'] . '(' . json_encode($data) . ');';
 	exit();
 }
 
